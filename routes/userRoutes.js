282252a4821113886aff9e058c55069edbe00c4d
Edutlan <andresarteagaramos@gmail.com>
2024-11-06 09:29:02 -0500

    d

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import { getUserProfile } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/profile/:userId', protect, getUserProfile);
+router.get('/profile/:userId', getUserProfile);
 
 export default router;
