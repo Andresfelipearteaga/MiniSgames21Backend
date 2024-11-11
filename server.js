1a10b8e2e52c279227f454ed2a4ca397154c2aa1
Edutlan <andresarteagaramos@gmail.com>
2024-11-11 15:16:48 -0500

    cambiar puero

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/activities', activityRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
