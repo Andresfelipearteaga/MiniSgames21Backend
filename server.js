6c98a742e4d88fa00d9c15ef5229f6b124928598
Edutlan <andresarteagaramos@gmail.com>
2024-11-11 15:00:53 -0500

    agregar backend

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-app.use('/', init);
+app.use('/backend', init);
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/activities', activityRoutes);
